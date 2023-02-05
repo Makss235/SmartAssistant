e266a26e8459caaa3454afbea9a90e4eaa6b386a
makss <makss@ASUSTG>
2023-02-05 23:20:19 +0300

    Создана базовая структура

---
+++ SmartAssistant/Windows/MainWindow.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace SmartAssistant.Windows
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Width = 500;
+            Height = 300;
+            Title = "Привет, Иван!";
+        }
+    }
+}
