2ea408c80830d31668334c48bdbffeb3027196a9
makss <makss@ASUSTG>
2023-02-05 17:45:39 +0300

    Создание тестовой WPF на основе консольного

--- Tests/TestConsoleWPF/Program.cs
+++ Tests/TestConsoleWPF/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Markup;
+
+namespace MySimpleProgramConsole2
+{
+    class MyWindow : Window
+    {
+        public MyWindow()
+        {
+            Width = 500;
+            Height = 300;
+            Title = "My Simple Window";
+            Grid grid = new Grid();
+            IAddChild add = grid;
+            Button button = new Button();
+            button.Content = "hhhh";
+            add.AddChild(button);
+            IAddChild addChild = this;
+            addChild.AddChild(grid);
+        }
+    }
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Window myWin = new MyWindow();
+            myWin.Show();
+            Application myApp = new Application();
+            myApp.Run();
+        }
+    }
+}
