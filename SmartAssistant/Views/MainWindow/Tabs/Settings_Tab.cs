f766d8f908c7a372ed82ef4712123646eb7c62c7
makss <makss@ASUSTG>
2023-02-07 00:26:03 +0300

    Создание вкладки настроек

---
+++ SmartAssistant/Views/MainWindow/Tabs/Settings_Tab.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Markup;
+
+namespace SmartAssistant.Views.MainWindow.Tabs
+{
+    public class Settings_Tab : UserControl
+    {
+        public Settings_Tab()
+        {
+            TextBlock textBlock = new TextBlock()
+            {
+                Text = "gggggggggggggggggggggggggg",
+                FontSize = 20,
+                Foreground = MainWindow.BackgroundLightBrush
+            };
+
+            Grid grid = new Grid();
+            grid.Children.Add(textBlock);
+            grid.Margin = new Thickness(20, 0, 0, 0);
+
+            Border mainBorder = new Border()
+            {
+                CornerRadius = new CornerRadius(0, 20, 20, 0),
+                Background = MainWindow.BackgroundDarkBrush,
+                Margin = new Thickness(-25, 0, 0, 0),
+                Height = 500,
+                Width = 535
+            };
+            mainBorder.Child = grid;
+        }
+    }
+}
