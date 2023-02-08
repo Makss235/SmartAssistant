c1356863ad13e8faffbea16c0067c3805f289e89
makss <makss@ASUSTG>
2023-02-09 01:13:40 +0300

    Начало добавления json-файлов

---
+++ SmartAssistant/Data/Speech/JsonObjects/ProgramNamesObj.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SmartAssistant.Data.Speech.JsonObjects
+{
+    public class ProgramNamesObj
+    {
+        public string Name { get; set; }
+        public List<string> CallingName { get; set; }
+        public string Path { get; set; }
+    }
+}
---
+++ SmartAssistant/Data/Speech/ProgramNames.cs
@@ -0,0 +1,52 @@
+﻿using SmartAssistant.Data.Speech.JsonObjects;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+
+namespace SmartAssistant.Data.Speech
+{
+    public static class ProgramNames
+    {
+        private static string language;
+
+        private static string jsonsDirectory;
+        private static string fileName;
+        private static string fullPathFileName;
+
+        public static List<ProgramNamesObj> ProgramNamesObjs { get; private set; }
+
+        public static void Init(string Language)
+        {
+            language = Language;
+
+            jsonsDirectory = Path.Combine(string.Format($"c:\\users\\{Environment.UserName}\\"),
+                "SmartAssistant\\Resources", "Json\\ProgramNames");
+            fileName = $"programNames_{language.ToLower()}.json";
+            fullPathFileName = Path.Combine(jsonsDirectory, fileName);
+
+            Deserialize();
+        }
+
+        public static void Deserialize()
+        {
+            string allTextFronJson = File.ReadAllText(fullPathFileName);
+            ProgramNamesObjs = JsonSerializer.Deserialize<List<ProgramNamesObj>>(allTextFronJson);
+        }
+
+        public async static void Serialize(List<ProgramNamesObj> programNamesObjsList)
+        {
+            File.WriteAllText(fullPathFileName, string.Empty);
+            using (FileStream fs = new FileStream(fullPathFileName, FileMode.OpenOrCreate))
+            {
+                var options = new JsonSerializerOptions()
+                {
+                    Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                    WriteIndented = true
+                };
+                await JsonSerializer.SerializeAsync(fs, programNamesObjsList, options);
+            }
+        }
+    }
+}
