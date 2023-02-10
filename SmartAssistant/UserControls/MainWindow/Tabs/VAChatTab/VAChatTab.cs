3366b8e2b4fb4d521319b6cc147557f1d5c10d7f
makss <makss@ASUSTG>
2023-02-11 01:20:22 +0300

    Разметил каркас VAChatTab

--- SmartAssistant/UserControls/MainWindow/Tabs/VAChatTab/VAChatTab.cs
+++ SmartAssistant/UserControls/MainWindow/Tabs/VAChatTab/VAChatTab.cs
@@ -14,15 +14,43 @@ namespace SmartAssistant.UserControls.MainWindow.Tabs.VAChatTab
             Height = height;
             Visibility = visibility;
 
-            TextBlock textBlock = new TextBlock();
-            textBlock.Text = "jjjjjjjjjjj";
-            textBlock.FontSize = 20;
+            ScrollViewer scrollViewer = new ScrollViewer()
+            {
+                Margin = new Thickness(0, 40, 0, 100),
+            };
+
+            TextBox typingMessageTextBox = new TextBox()
+            {
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Bottom,
+                Margin = new Thickness(20),
+                TextWrapping = TextWrapping.Wrap,
+                VerticalContentAlignment = VerticalAlignment.Center,
+                Padding = new Thickness(15, 0, 15, 0),
+                FontSize = 16,
+                FontFamily = new FontFamily("Segoe UI Semibold"),
+                Width = Width - 150,
+                Height = 50
+            };
+
+            Button sendMessageButton = new Button()
+            {
+                VerticalAlignment = VerticalAlignment.Bottom,
+                HorizontalAlignment = HorizontalAlignment.Right,
+                Height = 50,
+                Width = 50,
+                Margin = new Thickness(20, 20, 30, 20),
+                BorderBrush = new SolidColorBrush(Colors.Black),
+                BorderThickness = new Thickness(1)
+            };
 
             Grid mainGrid = new Grid()
             {
                 Margin = new Thickness(20, 0, 0, 0)
             };
-            mainGrid.Children.Add(textBlock);
+            mainGrid.Children.Add(scrollViewer);
+            mainGrid.Children.Add(typingMessageTextBox);
+            mainGrid.Children.Add(sendMessageButton);
 
             Border mainBorder = new Border()
             {
