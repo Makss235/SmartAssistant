7bc2f733e6995e80732dd704c8ab498500a817b6
MrVeseR <ViValKuzya1107@gmail.com>
2023-02-18 20:06:31 +0300

    сделал MessageChatStyle

--- SmartAssistant/Infrastructure/Styles/MainWindow/Tabs/VAChatTab/MessageChatStyle.cs
+++ SmartAssistant/Infrastructure/Styles/MainWindow/Tabs/VAChatTab/MessageChatStyle.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Media;
 using SmartAssistant.Windows;
 
@@ -9,38 +10,30 @@ namespace SmartAssistant.Infrastructure.Styles.MainWindow.Tabs.VAChatTab
     {
         public MessageChatStyle(SolidColorBrush messageBackground,
                                 SolidColorBrush messageForeground,
+                                SolidColorBrush messageBorderBrush,
                                 string message)
         {
-            FrameworkElementFactory textBoxFactory = new FrameworkElementFactory(typeof(TextBox));
-            textBoxFactory.SetValue(TextBox.BorderThicknessProperty, new Thickness(0));
-            textBoxFactory.SetValue(TextBox.BackgroundProperty, BasicColors.TransparentBrush);
-            textBoxFactory.SetValue(TextBox.TextProperty, message);
-            textBoxFactory.SetValue(TextBox.MarginProperty, new Thickness(5, 5, 5, 7));
+            FrameworkElementFactory textBoxFactory = new FrameworkElementFactory(typeof(TextBlock));
+            textBoxFactory.SetValue(TextBlock.BackgroundProperty, BasicColors.TransparentBrush);
+            textBoxFactory.SetValue(TextBlock.TextProperty, message);
+            textBoxFactory.SetValue(TextBlock.MarginProperty, new Thickness(10, 5, 10, 7));
+            
 
             FrameworkElementFactory messageTextBoxFactory = new FrameworkElementFactory(typeof(Border));
             messageTextBoxFactory.SetValue(Border.BackgroundProperty, messageBackground);
             messageTextBoxFactory.SetValue(Border.CornerRadiusProperty, new CornerRadius(10));
-            messageTextBoxFactory.SetValue(Border.BorderBrushProperty, Application.Current.Resources["ButtonMouseOverBrush"]);
+            messageTextBoxFactory.SetValue(Border.BorderBrushProperty, messageBorderBrush);
             messageTextBoxFactory.SetValue(Border.BorderThicknessProperty, new Thickness(2));
             messageTextBoxFactory.SetValue(Border.MinWidthProperty, (double)15);
             messageTextBoxFactory.SetValue(Border.MaxWidthProperty, (double)450);
             messageTextBoxFactory.SetValue(Border.RenderTransformOriginProperty, new Point(1, 1));
             messageTextBoxFactory.AppendChild(textBoxFactory);
 
-            Trigger isEnabledTrigger = new Trigger
-            {
-                Property = TextBox.IsEnabledProperty,
-                Value = false
-            };
-            isEnabledTrigger.Setters.Add(new Setter(TextBox.OpacityProperty, (double)1));
-
             TargetType = typeof(TextBox);
-            Triggers.Add(isEnabledTrigger);
             Setters.Add(new Setter(TextBox.BackgroundProperty, Application.Current.Resources["Transparent"]));
             Setters.Add(new Setter(TextBox.ForegroundProperty, messageForeground));
             Setters.Add(new Setter(TextBox.BorderThicknessProperty, new Thickness(0)));
             Setters.Add(new Setter(TextBox.MarginProperty, new Thickness(8, 5, 8, 5)));
-            Setters.Add(new Setter(TextBox.IsEnabledProperty, false));
             Setters.Add(new Setter(TextBox.TextWrappingProperty, TextWrapping.Wrap));
             Setters.Add(new Setter(TextBox.FontSizeProperty, (double)18));
             Setters.Add(new Setter(TextBox.FontFamilyProperty, new FontFamily("Segoe UI Semibold")));
