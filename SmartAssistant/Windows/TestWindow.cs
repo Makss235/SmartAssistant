e76927fba986762e43b1c2c1a2bf1a402f839cb7
MrVeseR <ViValKuzya1107@gmail.com>
2023-04-16 12:30:03 +0300

    Тестовый myPolygon

--- SmartAssistant/Windows/TestWindow.cs
+++ SmartAssistant/Windows/TestWindow.cs
@@ -1,17 +1,62 @@
 ﻿using SmartAssistant.UserControls.Widgets;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace SmartAssistant.Windows
 {
     public class TestWindow : Window
     {
+        Path a;
+        Polygon myPolygon;
         public TestWindow()
         {
             SExpander sExpander = new SExpander();
             sExpander.HorizontalAlignment = HorizontalAlignment.Left;
             sExpander.VerticalAlignment = VerticalAlignment.Top;
-            Content = sExpander;
+
+            Line myLine = new Line
+            {
+                Stroke = Brushes.Green,
+                X1 = 50, Y1 = 50,
+                X2 = 100, Y2 = 100,
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Top,
+                StrokeThickness = 3,
+            };
+
+            myPolygon = new Polygon
+            {
+                Stroke = Brushes.Blue,
+                Fill = Brushes.Pink,
+                StrokeThickness = 3,
+                Stretch = Stretch.Fill,
+                Height = 50, Width = 50,
+                HorizontalAlignment = HorizontalAlignment.Left,
+                VerticalAlignment = VerticalAlignment.Top,
+                Points = new PointCollection { new Point(0, 0), new Point(1, 0), new Point(1, 1) },
+                Margin = new Thickness(50),
+                RenderTransformOrigin = new Point(100, 100),
+                LayoutTransform = new RotateTransform { Angle = 45 }
+            };
+            myPolygon.PreviewMouseLeftButtonDown += A_P;
+
+            Grid grid = new Grid
+            {
+                Children = {  new TextBox() { Margin = new Thickness(200)}, myPolygon }
+            };
+
+            Content = grid;
+        }
+
+        private void A_P(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            a.Fill = Brushes.Green;
+            if (myPolygon.Fill == Brushes.Pink)
+                myPolygon.Fill = Brushes.Green;
+            else if (myPolygon.Fill == Brushes.Green)
+                myPolygon.Fill = Brushes.Pink;
         }
     }
 }
