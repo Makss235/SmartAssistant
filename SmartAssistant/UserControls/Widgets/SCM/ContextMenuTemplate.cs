425f9cb4278c71fe8e6282026c46a152b5e1f104
MrVeseR <ViValKuzya1107@gmail.com>
2023-04-22 20:28:35 +0300

    Добавил itemPresenter в тэмплейт

--- SmartAssistant/UserControls/Widgets/SCM/ContextMenuTemplate.cs
+++ SmartAssistant/UserControls/Widgets/SCM/ContextMenuTemplate.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace SmartAssistant.UserControls.Widgets.SCM
 {
@@ -13,9 +14,13 @@ namespace SmartAssistant.UserControls.Widgets.SCM
     {
         public ContextMenuTemplate()
         {
+            FrameworkElementFactory itemsPresenterF = new FrameworkElementFactory(typeof(ItemsPresenter));
+
             FrameworkElementFactory factory = new FrameworkElementFactory(typeof(BackgroundBorder));
             factory.SetValue(Border.WidthProperty, (double)200);
             factory.SetValue(Border.HeightProperty, (double)200);
+
+            factory.AppendChild(itemsPresenterF);
 
             VisualTree = factory;
         }
